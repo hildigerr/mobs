1608aca8bd69fff93980f50864ffa8fdc3eea8a1
PilzAdam <pilzadam@minetest.net>
2013-04-12 18:36:48 +0200

    Add model for stone monster

--- monsters/stone_monster.lua
+++ monsters/stone_monster.lua
@@ -1,3 +1,4 @@
+local USE_SPRITES = minetest.settings:get_bool("mobs.use_sprites", false)
 
 mobs:register_mob("mobs:stone_monster", {
     type = "monster",
@@ -14,11 +15,22 @@ mobs:register_mob("mobs:stone_monster", {
     light_damage = 0,
     light_resistant = true,
 
-    visual = "upright_sprite",
+    visual = USE_SPRITES and "upright_sprite" or "mesh",
     drawtype = "front",
-    visual_size = {x=1, y=2},
-    collisionbox = {-0.4, -1, -0.4, 0.4, 1, 0.4},
-    textures = {"mobs_stone_monster.png", "mobs_stone_monster_back.png"},
+    mesh = "mobs_stone_monster.x",
+    visual_size = USE_SPRITES and {x=1, y=2} or {x=3, y=2.6},
+    collisionbox = USE_SPRITES and {-0.4, -1, -0.4, 0.4, 1, 0.4} or {-0.4, 0, -0.4, 0.4, 1.9, 0.4},
+    textures = USE_SPRITES and {"mobs_stone_monster.png", "mobs_stone_monster_back.png"} or {"mobs_stone_monster_mesh.png"},
+    animation = not USE_SPRITES and {
+        speed_normal = 15,
+        speed_run = 15,
+        stand_start = 0,
+        stand_end = 14,
+        walk_start = 15,
+        walk_end = 38,
+        run_start = 40,
+        run_end = 63,
+    },
 
     makes_footstep_sound = true,
 
