927d4e577d44b2b176121ee594d507ce26333a55
PilzAdam <pilzadam@minetest.net>
2013-04-29 13:52:51 +0200

    Add tree monster mesh

--- monsters/tree_monster.lua
+++ monsters/tree_monster.lua
@@ -1,3 +1,4 @@
+local USE_SPRITES = minetest.settings:get_bool("mobs.use_sprites", false)
 
 mobs:register_mob("mobs:tree_monster", {
     type = "monster",
@@ -14,11 +15,24 @@ mobs:register_mob("mobs:tree_monster", {
     light_resistant = true,
     disable_fall_damage = true,
 
-    visual = "upright_sprite",
+    visual = USE_SPRITES and "upright_sprite" or "mesh",
     drawtype = "front",
-    visual_size = {x=1, y=2},
-    collisionbox = {-0.4, -1, -0.4, 0.4, 1, 0.4},
-    textures = {"mobs_tree_monster.png", "mobs_tree_monster_back.png"},
+    mesh = "mobs_tree_monster.x",
+    visual_size = USE_SPRITES and {x=1, y=2} or {x=4.5,y=4.5},
+    collisionbox = USE_SPRITES and {-0.4, -1, -0.4, 0.4, 0.9, 0.4} or {-0.4, -0.01, -0.4, 0.4, 1.9, 0.4},
+    textures = USE_SPRITES and {"mobs_tree_monster.png", "mobs_tree_monster_back.png"} or {"mobs_tree_monster_mesh.png"},
+    animation = not USE_SPRITES and {
+        speed_normal = 15,
+        speed_run = 15,
+        stand_start = 0,
+        stand_end = 24,
+        walk_start = 25,
+        walk_end = 47,
+        run_start = 48,
+        run_end = 62,
+        punch_start = 48,
+        punch_end = 62,
+    },
 
     makes_footstep_sound = true,
 
