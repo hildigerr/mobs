f0f91c66e7df023a8c924f3825abbf681f980c9a
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2020-04-28 00:16:28 -0700

    enable turtles

--- animals/turtle.lua
+++ animals/turtle.lua
@@ -1,23 +1,22 @@
+local turtle_setting = minetest.settings:get("mobs.turtles") or "disabled"
 
 --turtle: 1-50 doing nothing, 60-90 running, 95-100 hidden, 105-135 swimming
 
-mobs:register_mob("mobs:turtle", {
+mobs:register_mob("turtle", {
     type = "animal",
 
-    hp_max = 2,
-    armor = 200,
+    hp_max = 12,
+    armor = {crumbly = 5, cracky = 25, choppy = 50, fleshy = 60},
     walk_velocity = 1,
 
-    water_damage = 0,
-    lava_damage = 1,
-    light_damage = 0,
+    damage = {fall = 1, water = 0, lava = 6, light = 0},
 
     visual = "mesh",
     drawtype = "front",
     mesh = "mobs_turtle.x",
     visual_size = {x=1.5, y=1.5},
     collisionbox = {-0.7, -0.01, -0.4, 0.7, 1.4, 0.4},
-    textures = {"mobs_turtle.png"},
+    textures = {"mobs_turtle_mesh.png"},
     animation = {
         speed_normal = 5,
         stand_start = 1,
@@ -32,36 +31,5 @@ mobs:register_mob("mobs:turtle", {
 
     drops = {},
 
-})
-
-mobs:register_mob("mobs:turtle_hidden", {
-    type = "animal",
-
-    hp_max = 2,
-    armor = 200,
-    walk_velocity = 1,
-
-    water_damage = 0,
-    lava_damage = 1,
-    light_damage = 0,
-
-    visual = "mesh",
-    drawtype = "front",
-    mesh = "mobs_turtle.x",
-    visual_size = {x=1.0, y=1.0},
-    collisionbox = {-0.7, -0.01, -0.4, 0.7, 1.4, 0.4},
-    textures = {"mobs_turtle.png"},
-    animation = {
-        speed_normal = 0,
-        stand_start = 95,
-        stand_end = 100,
-        walk_start = 60,
-        walk_end = 90,
-    },
-
-    makes_footstep_sound = false,
-
-    drops = {},
-
-})
+}, turtle_setting == "disabled")
 
